09f2a27062128bfea38c561a19687d5c88d9d48e
pradeeprawat-jft <pradeep.rawat@jellyfishtechnologies.com>
2023-10-18 16:48:19 +0530

    lambda policy added

---
+++ lambdapolicy/src/main/java/com/example/lambdapolicy/LambdapolicyApplication.java
@@ -0,0 +1,13 @@
+package com.example.lambdapolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdapolicyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdapolicyApplication.class, args);
+    }
+
+}
---
+++ lambdapolicy/src/test/java/com/example/lambdapolicy/LambdapolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lambdapolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdapolicyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
